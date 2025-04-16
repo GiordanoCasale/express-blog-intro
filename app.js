1abe0e6556a895ac200e11a548c4988dab4cbb62
GiordanoCasale <giordanocasale8@gmail.com>
2025-04-16 16:46:29 +0200

    [UPD] imported express

---
+++ app.js
@@ -0,0 +1,8 @@
+//importazione di express
+const express = require("express");
+
+//inizializzo express dentro la variabile app
+const app = express();
+
+//definizione del numero di porta
+const port = 3000;
